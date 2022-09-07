c546a4cbde54247be8019f15e2442e9c6344a385
Colleen Murray <colleenkhm@gmail.com>
2022-09-07 09:19:05 -0500

    gh pages deploy

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -4,7 +4,8 @@ function Stories() {
     return (
         <div class="tell-story-container">
             <h3>Tell us your story</h3>
-            <p>Do you have a story about your experience navigating the healthcare system as a Woman of Color that you'd like to share, either to us or to others? </p>
+            <p>Do you have a story about your experience navigating the healthcare system as a Woman of Color that you'd like to share, either just to us or to others as well? </p>
+            <p>Note: there is an option to post your submission on the site anonymously, as well as an option for us not to post it at all. All submissions are reviewed by a member of the board before publication.</p>
         </div>
     )
 }
