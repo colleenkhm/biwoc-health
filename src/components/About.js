05d4cb7063dfadcdd8d9d9d9013da234cd18c389
Colleen Murray <colleenkhm@gmail.com>
2022-09-07 08:35:06 -0500

    gh-pageS

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,14 @@ import React from 'react';
 function About() {
     return (
         <main className="about-page">
+            <section title="general-about" className="about-section">
+                <h2 className="title">ABOUT</h2>
+                <p>BIWOC health is a nonprofit organization for Black, Indigenous, and Women of Color who are affected by the racial biases in the health care system.
+                </p>
+            </section>
             <section title="mission" className="about-section">
                 <h2 className="title">MISSION</h2>
-                <p>BIWOC health is a nonprofit organization for Black, Indigenous, and Women of Color who are affected by the racial biases in the health care system. To advocate, spread awareness, and execute our vision.
+                <p>To advocate, spread awareness, and execute our vision.
                 </p>
             </section>
             <section title="vision" className="about-section">
